b285e967221fad6abcd8313c6d909af285c637ab
HandelFan <jegan.sivakasiulaganathan@gmail.com>
2022-03-13 14:37:52 -0400

    Added ElementSwapping

---
+++ c#projects/elementswapping/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+// We don’t provide test cases in this language yet, but have outlined the signature for you. Please write your code below, and don’t forget to test edge cases!
+class ElementSwapping {
+  static void Main(string[] args) {
+    // Call findMinArray() with test cases here
+  }
+  
+  private static int[] findMinArray(int[] arr, int k) {
+    // Write your code here
+    return new int[0];
+  }
+}
